eb0c8f3bf7f805703e6d650156eae1084c775f88
TAHA-CODEXX <tahastudying@gmail.com>
2025-12-10 16:56:03 +0100

    the app.jsx Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from "react";
-// import Home from './pages/home/Home.jsx'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Corbeille from "./pages/corbeille/corbeille";
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/home/Home';
+import Corbeille from './pages/corbeille/corbeille';
+import './App.css';
+
+
+function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/corbeille" element={<Corbeille />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <DndProvider backend={HTML5Backend}>
+        <div className="app">
+          <Routes>
+            <Route path="/login" element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/corbeille" element={<Corbeille />} />
+          </Routes>
+          <Toaster position="top-center" />
+        </div>
+      </DndProvider>
+    </Router>
   );
 }
+
+export default App;
