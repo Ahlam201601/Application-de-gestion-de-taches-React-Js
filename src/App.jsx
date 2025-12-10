d39294940b96792056b6ea6950b4cfeffc7ac862
TAHA-CODEXX <tahastudying@gmail.com>
2025-12-10 16:46:54 +0100

    solve some problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Home from './pages/home/Home.jsx'
+import Home from '../src/pages/home/Home'
 export default function App() {
   return (
-    <div>App</div>
+   <Home/>
   )
 }
 
