4895c3d1f49b907d5b56759d46cc1113526640ed
TAHA-CODEXX <tahastudying@gmail.com>
2025-12-11 11:43:55 +0100

    add the api for the add and show the tasks

--- Api.js
+++ Api.js
@@ -3,6 +3,29 @@ import axios from "axios";
 const API_URL = "http://localhost:3001";
 
 
+// Récupérer toutes les tâches
+export const getTasks = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/tasks`);
+    return response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des tâches:', error);
+    return [];
+  }
+};
+
+// Créer une nouvelle tâche
+export const createTask = async (task) => {
+  try {
+    const response = await axios.post(`${API_URL}/tasks`, task);
+    return response.data;
+  } catch (error) {
+    console.error('Erreur lors de la création de la tâche:', error);
+    throw error;
+  }
+};
+
+
 export const getTrash = async () => {
   try {
     const res = await axios.get(`${API_URL}/trash`);
@@ -31,7 +54,7 @@ export const deleteTaskForever = async (id) => {
     console.error("Erreur lors de la suppression définitive :", error);
   }
 };
-//uupdate task
+
 export const updateTask = async (id, task) =>{
     try {
         const response = await axios.put(`${API_URL}/tasks/${id}`, task);
