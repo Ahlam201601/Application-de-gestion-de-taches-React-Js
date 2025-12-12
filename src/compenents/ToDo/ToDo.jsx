9b9615e5dd88ed161a20adfae9eecaae85af9f32
ahlam201601 <elhadriahlam9@gmail.com>
2025-12-12 12:07:27 +0100

    drop components TO DO

--- src/compenents/ToDo/ToDo.jsx
+++ src/compenents/ToDo/ToDo.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useDrop } from 'react-dnd';
+import { FaCircle } from 'react-icons/fa';
+import './ToDo.css';
 
-const ToDo = () => {
+const ToDo = ({ tasks, onEdit, onDrop}) => {
+  const [{ isOver }, drop] = useDrop({
+    accept: 'task',
+    drop: (item) => {
+      if (item.status !== 'todo') {
+        onDrop(item.id, 'todo');
+      }
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
+  });
+
+  
+
   return (
-    <div>ToDo</div>
-  )
-}
+    <div ref={drop} className={`todo-column ${isOver ? 'drag-over' : ''}`}>
+      <div className="column-header">
+        <h2>To Do</h2>
+        <div className="task-count-badge">
+          <FaCircle className="count-icon" />
+          <span>{tasks.length}</span>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
 
-export default ToDo
+export default ToDo;
